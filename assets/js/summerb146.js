6b356554096abeecc846afe5aeb56816bcf0daf3
Ivan Manolov <ivanmanolov90@gmail.com>
2015-07-29 19:50:32 -0400

    initial changes like color theme change and a bit of personalization

--- assets/js/summerb146.js
+++ assets/js/summerb146.js
@@ -17,7 +17,7 @@ var summer = (function ($) {
             var postPos = $(this).offset().top;
             var topOfWindow = $(window).scrollTop(),
                 windowHeight = $(window).height();
-                if (postPos < topOfWindow + (windowHeight/ 1.4)) {
+                if (postPos < topOfWindow + (windowHeight/ 1.0)) {
                     $(this).addClass('fadeInDown');
                 }
             });
